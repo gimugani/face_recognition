357dfeb0ba2b08ec9590c127e9a1bbdb4d707291
Kwan Kim <gimugani@gmail.com>
2019-10-09 10:26:03 -0700

    upload to heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -19,7 +19,7 @@ class Register extends React.Component {
         this.setState({password: event.target.value});
     }
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://enigmatic-cliffs-62132.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({
